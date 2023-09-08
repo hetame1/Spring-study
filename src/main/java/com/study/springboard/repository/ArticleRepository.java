9f2ae586698187c31a100c48aa461960b9567d2d
hetame1 <poi8946@naver.com>
2023-09-08 23:32:26 +0900

    #12 - repository interface 작성
    이걸로 db 에 접근

---
+++ src/main/java/com/study/springboard/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.study.springboard.repository;
+
+import com.study.springboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
